5763240df3c3a3ce84517897606bdbb582a8f112
Mber <mberthiaume@broncos.uncfsu.edu>
2025-11-23 11:07:21 -0800

    Create file_handler.py

---
+++ file_handler.py
@@ -0,0 +1,19 @@
+
+ import os
+ import json
+ 
+ def write_bytes(path, data):
+     with open(path, "wb") as f:
+         f.write(data)
+ 
+ def read_bytes(path):
+     with open(path, "rb") as f:
+         return f.read()
+ 
+ def write_json(path, data):
+     with open(path, "w") as f:
+         json.dump(data, f, indent=4)
+ 
+ def read_json(path):
+     with open(path, "r") as f:
+         return json.load(f)
